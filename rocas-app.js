56780d016b0cd87fb209e502f603aee02703d946
Verónica E. Álvarez <45977740+vealvarez@users.noreply.github.com>
2025-02-03 18:23:06 -0300

    030225.18

--- rocas-app.js
+++ rocas-app.js
@@ -33,12 +33,15 @@ document.addEventListener('DOMContentLoaded', () => {
         if (score <= 1) {
             riskAssessment.textContent = 'Riesgo bajo de infección por KPC';
             riskAssessment.style.color = 'green';
+            riskAssessment.style.fontSize = '20px';
         } else if (score > 1 && score <= 3) {
             riskAssessment.textContent = 'Riesgo moderado de infección por KPC';
             riskAssessment.style.color = 'orange';
+            riskAssessment.style.fontSize = '20px';
         } else {
             riskAssessment.textContent = 'Riesgo alto de infección por KPC';
             riskAssessment.style.color = 'red';
+            riskAssessment.style.fontSize = '20px';
         }
     }
 });
